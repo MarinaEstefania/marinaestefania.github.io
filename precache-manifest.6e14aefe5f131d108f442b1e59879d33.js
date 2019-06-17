d819a452f4213af0f8a9e1f194e60554ea94b235
MarinaEstefania <m.garciac11@gmail.com>
2019-06-17 12:20:16 -0500

    Updates

---
+++ precache-manifest.6e14aefe5f131d108f442b1e59879d33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4317ed6ea3824ef2ac682ce759f1982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72ae32a66833124b545b",
+    "url": "/static/css/main.6b871608.chunk.css"
+  },
+  {
+    "revision": "8a6b5f1364b87b8e61d8",
+    "url": "/static/js/2.dc3c908d.chunk.js"
+  },
+  {
+    "revision": "72ae32a66833124b545b",
+    "url": "/static/js/main.535fdc11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "808eb8146952552099fd9b89554650fe",
+    "url": "/static/media/about.808eb814.jpg"
+  },
+  {
+    "revision": "bb7bc7258405e8741c2082190f64a786",
+    "url": "/static/media/liver.bb7bc725.jpeg"
+  },
+  {
+    "revision": "0ff55f0711712f382221c3e8e68f062e",
+    "url": "/static/media/mundodulce.0ff55f07.jpeg"
+  },
+  {
+    "revision": "e9ad6205bd47eab90e37152b6568fdf1",
+    "url": "/static/media/pokegoal.e9ad6205.jpeg"
+  },
+  {
+    "revision": "ee1115bffd6545eacd7633794db452a2",
+    "url": "/static/media/screen (5).ee1115bf.jpg"
+  }
+]);
