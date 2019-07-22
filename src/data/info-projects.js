99fe36094dd4fe085bee4cec133eb215729c281b
MarinaEstefania <m.garciac11@gmail.com>
2019-07-22 09:40:59 -0500

    add info projects in a json file

---
+++ src/data/info-projects.js
@@ -0,0 +1,26 @@
+
+
+{
+    projects: [
+      {
+        "id": "Organa",
+        "img": "./img/organamu.jpg",
+        "description": "description text",
+        "stack": "stack text",
+        "role": "role text",
+        "platform": "platform text",
+        "website": "http://www.google.com",
+        "github": "http://www.google.com"
+      },
+      {
+        "id": "BURGER",
+        "img": "src={organa}",
+        "description": "description text",
+        "stack": "stack text",
+        "role": "role text",
+        "platform": "platform text",
+        "website": "http://www.google.com",
+        "github": "http://www.google.com"
+      }
+    ]
+  }
